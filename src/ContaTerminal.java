605e85effcf45561ed747f6dacbfef4a02e23936
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-09-27 13:37:28 -0300

    Declarando as variaveis referentes aos dados da conta do usuario

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,10 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+
     }
 }
